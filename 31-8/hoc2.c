6e19c4393db0cd3570594c330ba8a1d2f4ce3537
hocpro261102 <hocpro261102@gmail.com>
2021-09-14 21:29:56 +0700

    today

--- 31-8/hoc2.c
+++ 31-8/hoc2.c
@@ -1,49 +1,54 @@
 #include <stdio.h>
-void nhapmang(int a[],int n)
+#include <string.h>
+struct SV
 {
-    for(int i=0;i<n;i++)
-        {
-            printf("nhap phan tu thu %d:",i+1);
-            scanf("%d",&a[i]);
-        }
-}
-
-void xuatmang(int a[],int n)
-{
-    for(int i=0;i<n;i++)
-        {
-            
-            printf("%d \t",a[i]);
-        }
-}
-void daonguoc(int a[],int n)
-{
-     
-   for(int i=0; i< n/2; i++)
-    {
-        int x = n - i - 1;
-        int y = a[x];
-        a[x] = a[i];
-        a[i] = y;
-    }
-
-}
-
-
-
-
+    char ten[30];
+    int toan,ly,hoa,dtb;
+};
 int main()
 {
-    int a[100],b[100];
-    int n;
-    printf("nhap so phan tu cua mang:");
+    int n,i;
+    printf("nhap so hoc sinh:");
     scanf("%d",&n);
-    nhapmang(a,n);
-    xuatmang(a,n);
-    daonguoc(a,n);
-    printf("\n");
-    xuatmang(a,n);
+    struct SV sv[n];
+    for(i=0;i<n;i++)
+    {
+        printf("Nhap ten hoc sinh thu %d:",i+1);
+        scanf("%s",sv[i].ten);
+        printf("\nNhap diem toan:");
+        scanf("%d",&sv[i].toan);
+        printf("\nNhap diem ly:");
+        scanf("%d",&sv[i].ly);
+        printf("\nNhap diem hoa:");
+        scanf("%d",&sv[i].hoa);
+        sv[i].dtb=(sv[i].toan+sv[i].ly+sv[i].hoa)/3;
+    }
     
-
+    
+    for(i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+                {
+                    
+                    if(sv[i].dtb<sv[j].dtb)
+                        {
+                        int temp;
+                        temp=sv[j].dtb;
+                        sv[j].dtb=sv[i].dtb;
+                        sv[i].dtb=temp;
+                        char tem[30];
+                        strcpy(tem,sv[j].ten);
+                        strcpy(sv[j].ten,sv[i].ten);
+                        strcpy(sv[i].ten,tem);
+                        }
+                }
+        }
+    for(i=0;i<3;i++)
+    {
+        printf("\n %d.Ten hoc sinh: %s",i+1,sv[i].ten);
+        printf("\n Diem trung binh la: %d",sv[i].dtb);
+        
+    }
+    
     return 0;
 }
